2240e554fb48941f7615cab348502b1823159c45
Gabriel Downs <gndowns@gmail.com>
2018-05-05 12:08:11 -0400

    add start script

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 // use Heroku port
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req,res) {
   res.send('Hello Reuter!');
 });
 
-app.listen(port, function() {
-  console.log('Listening on port ' + port);
+app.listen(PORT, function() {
+  console.log('Listening on port ' + PORT);
 });
