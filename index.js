0f59025f042aafbfba35ce419a4dfeb6b590f2c1
Gabriel Downs <gndowns@gmail.com>
2018-05-05 12:00:03 -0400

    use heroku env port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+// use Heroku port
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req,res) {
   res.send('Hello Reuter!');
 });
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
 });
