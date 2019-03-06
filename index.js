eb319e6af5c34f0dd68d61662b93cc2c262288cf
Gabriel Downs <gndowns@gmail.com>
2019-03-06 02:00:02 -0500

    include videos in random ordering

--- index.js
+++ index.js
@@ -15,15 +15,31 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 
 
-// on server startup, load list of all images in static directory
-// keep list of image filenames as global array variable
-IMAGE_NAMES = fs.readdirSync(__dirname + '/public/images');
+// on server startup, load list of all images & vidoeos in static directory
+// into global array variable
+FILE_NAMES = [];
+// load photos and gifs first
+fs.readdirSync(__dirname + '/public/images').forEach(function(img_name) {
+  FILE_NAMES.push({
+    // for each file, track whether it is a photo or video
+    'ftype': 'img',
+    'src': img_name
+  });
+});
+// next load videos
+fs.readdirSync(__dirname + '/public/videos').forEach(function(vid_name) {
+  FILE_NAMES.push({
+    // for each file, track whether it is a photo or video
+    'ftype': 'video',
+    'src': vid_name
+  });
+});
 
 
 app.get('/', function(req,res) {
   // shuffle order of image names before sending to client
-  shuffle(IMAGE_NAMES);
-  res.render('index.html', {image_names: IMAGE_NAMES});
+  shuffle(FILE_NAMES);
+  res.render('index.html', {file_names: FILE_NAMES});
 });
 
 app.listen(PORT, function() {
