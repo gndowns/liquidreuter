2868bdd383213f7e695ddab43138d571657617c9
Gabriel Downs <gndowns@gmail.com>
2019-03-06 01:22:30 -0500

    dynamically load images using ejs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const fs = require('fs');
+
 
 // use Heroku port
 const PORT = process.env.PORT || 3000;
@@ -12,9 +14,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 
 
+// on server startup, load list of all images in static directory
+// keep list of image filenames as global array variable
+IMAGE_NAMES = fs.readdirSync(__dirname + '/public/images');
+
+
 app.get('/', function(req,res) {
-  res.render('index.html');
-  // res.send('Hello Reuter!');
+  res.render('index.html', {image_names: IMAGE_NAMES});
 });
 
 app.listen(PORT, function() {
